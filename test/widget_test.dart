3bc999e95165da5b899698f2dd0341e0afcd6211
penguix0 <90452358+penguix0@users.noreply.github.com>
2022-12-13 22:45:30 +0100

    Updated the project name

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:container_detection_web_app/main.dart';
+import 'package:container_detection_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
