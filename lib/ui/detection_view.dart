82f772237954d66b35c5a6a8843f3ba1ccb195e3
penguix0 <90452358+penguix0@users.noreply.github.com>
2022-12-14 17:36:10 +0100

    Reorganizing of the project

---
+++ lib/ui/detection_view.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DetectionView extends StatelessWidget {
+  const DetectionView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text("This is my other page"),
+      ),
+    );
+  }
+}
